a32fac5431fc09d3c4fb70b1ca54529fe3925ebb
Juank628 <jsanchez_zunino@yahoo.com>
2019-05-23 11:48:41 -0500

    print quote

--- js/script.js
+++ js/script.js
@@ -7,12 +7,7 @@ project 1 - A Random Quote Generator
 
 
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+  quotes array
 ***/
 
 var quotes = [
@@ -54,9 +49,7 @@ var quotes = [
 ];
 
 /***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
+  random quote
 ***/
 
 function getRandomQuote() {
@@ -65,29 +58,30 @@ function getRandomQuote() {
 }
 
 /***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
+  generate HTML and print in the DOM 
 ***/
 
 function printQuote() {
-  selectedQuote = getRandomQuote();
-  htmlQuote = `<p>"${selectedQuote.quote}"</p>`;
-  htmlDetails =
-    "<p>" +
-    selectedQuote.source  + ", " +
-    (selectedQuote.citation === undefined ? '' : selectedQuote.citation) + ", " +
-    (selectedQuote.year === undefined ? '' : selectedQuote.year) + 
-    "</p>";
-  htmlCategory = selectedQuote.category ? `<p>${selectedQuote.category}</p>` : null;
-  return htmlQuote + htmlDetails + htmlCategory
+  var selectedQuote = getRandomQuote();
+  var htmlToPrint = '';
+  
+  htmlToPrint += `<p class="quote"> ${selectedQuote.quote} </p>`;
+  htmlToPrint += `<p class="source"> ${selectedQuote.source}`;
+
+  if(selectedQuote.citation){
+    htmlToPrint += `<span class="citation"> ${selectedQuote.citation} </span>`;
+  }
+
+  if(selectedQuote.year){
+    htmlToPrint += ` <span class="year"> ${selectedQuote.year} </span>`;
+  }
+
+  htmlToPrint += '</p>';
+  
+  var quoteBox = document.getElementById('quote-box');
+  quoteBox.innerHTML = htmlToPrint;
+
+  return htmlToPrint
 }
 
 console.log(printQuote());
